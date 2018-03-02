355bb0c9948df670306e5809b0df2837f69af441
Julia Nissen <nisse053@umn.edu>
2018-03-02 11:58:04 -0600

    Updated thank you doodle

--- assets/javascript/blockHeightThanksScript.js
+++ assets/javascript/blockHeightThanksScript.js
@@ -5,6 +5,10 @@ $(window).ready( function() {
 	var newLeft = (Math.abs(thanksContainerWidth - thanksbuttonWidth))/2;
 	$('.homeButton').css("left", newLeft);
 
+	var doodleHeight = $('#doodle8').height();
+	var newBottom = (((doodleHeight/3) + 2) * -1);
+	$('#doodle8').css("bottom", newBottom);
+
 });
 
 $(window).resize( function() {
@@ -13,5 +17,9 @@ $(window).resize( function() {
 	var thanksContainerWidth = $('.thanksContainer').width();
 	var newLeft = (Math.abs(thanksContainerWidth - thanksbuttonWidth))/2;
 	$('.homeButton').css("left", newLeft);
+
+	var doodleHeight = $('#doodle8').height();
+	var newBottom = (((doodleHeight/3) + 2) * -1);
+	$('#doodle8').css("bottom", newBottom);
 
 })
